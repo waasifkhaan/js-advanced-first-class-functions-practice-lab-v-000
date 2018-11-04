bb59bb02f3e5f2e487c2d78a419dd2610abb016e
Wasif Khan <waasif.khaan@gmail.com>
2018-11-04 21:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
 // Code your solution in this file!
+
+const callback = function (el, i, arr) {
+  console.log(`el.name`);
+
+};
 function logDriverNames(drivers){
   return drivers.map(console.log(driver.name))
 
