9ba0dba8341008d9908c6b5d4b2fb3f29634025e
Wasif Khan <waasif.khaan@gmail.com>
2018-11-04 21:45:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
 // Code your solution in this file!
 
-const callback = function (el, i, arr) {
-  console.log(`el.name`);
 
-}
 function logDriverNames(drivers){
-
-return drivers.forEach(callback);
-
+  newArray = [];
+drivers.forEach(
+  newArray.push(driver.name);
+)
+return newArray;
 
 }
