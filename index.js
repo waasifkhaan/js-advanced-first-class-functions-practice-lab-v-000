919b83e7c581bda7654fb848436dee89fa0c8b67
Wasif Khan <waasif.khaan@gmail.com>
2018-11-04 21:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function cb (el) {
-  console.log(el.name);
-
-};
-
-const logDriverNames = function (drivers, cb) {
-  for (const el of drivers) {
-    return cb(el);
-  }
-};
 
 function logDriverNames (collection) {
   for (const user of collection) {
